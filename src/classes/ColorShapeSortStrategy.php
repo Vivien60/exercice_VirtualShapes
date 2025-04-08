599f3c22821c2381067b399b5c6c936208ff0c56
Vivien60 <phedras.ch@gmail.com>
2025-04-08 22:48:22 +0200

    fix

--- src/classes/ColorShapeSortStrategy.php
+++ src/classes/ColorShapeSortStrategy.php
@@ -10,7 +10,7 @@ class ColorShapeSortStrategy extends AbstractShapeSortStrategy
     {
         $rankForElt1 = array_search($elt1->getColor(), $this->eltOrder);
         $rankForElt2 = array_search($elt2->getColor(), $this->eltOrder);
-        if(!$rankForElt1 || !$rankForElt2) {
+        if($rankForElt1 === false || $rankForElt2 === false) {
             throw new \LogicException("One or more color is not in pre-sorted list");
         }
         if ($rankForElt1 == $rankForElt2) {
